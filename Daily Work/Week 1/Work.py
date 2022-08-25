bdae3e91dd972de402684859d189546058b46248
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2022-08-25 14:47:26 -0700

    Week 1 Work

--- Daily Work/Week 1/Work.py
+++ Daily Work/Week 1/Work.py
@@ -21,8 +21,10 @@ def fibo(n):
 
 # Recursibe code for solution to sequence
 # 1,3,9,27,81,...
+# Formuka: previous iteration *3
 
 def reco(n):
+    # base case 1
     if n == 0:
         return 1
     
@@ -30,4 +32,43 @@ def reco(n):
         return reco(n-1) * 3
 
 
-print(reco(8))
+#print(reco(8))
+
+#Factorial Recursive function
+# 0! = 1
+# 1! = 1
+# 2! = 2 * 1
+# 3! = 3 * 2! (2 * 1)
+
+# Formula: input number * (factorial of the input number - 1)
+
+def factorialsFun(n):
+    if n == 0:
+        return 1
+
+    if n == 1:
+        return 1
+    
+    else:
+        return (n * factorialsFun(n-1))
+
+
+#print(factorialsFun(3))
+
+
+# Towers of Hanoi recursive function
+# Given :(
+
+def tower(n, source, dest, temp):
+    if n==1:
+        print("Move disk from", source, "to ", dest+".")
+    
+    else:
+        tower(n-1, source, temp, dest)
+        tower(1, source, dest, temp)
+        tower(n-1, temp, dest, source)
+
+def hanoi(n):
+    tower(n, "A", "C", "B") 
+
+print(hanoi(3))
